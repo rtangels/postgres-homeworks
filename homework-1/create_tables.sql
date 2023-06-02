b6d97a99f8d80a018df2dfbeb366184328bbfc5e
rtangels <VanshinChaineezMaster@yandex.ru>
2023-06-03 01:05:03 +0300

    main

--- homework-1/create_tables.sql
+++ homework-1/create_tables.sql
@@ -3,25 +3,25 @@
 create table employees
 (
 employee_id int PRIMARY KEY,
-first_name varchar(20) not NULL,
-last_name varchar(20) not NULL,
-title varchar(20) not NULL,
+first_name varchar(50) not NULL,
+last_name varchar(50) not NULL,
+title varchar(50) not NULL,
 birth_date date,
 notes text
 )
 
 create table customers
 (
-customer_id varchar(20) not NULL UNIQUE,
-company_name varchar(30) not NULL,
-contact_name varchar(20) not NULL
+customer_id varchar(50) not NULL UNIQUE,
+company_name varchar(50) not NULL,
+contact_name varchar(50) not NULL
 )
 
 create table orders
 (
 order_id int PRIMARY KEY,
-customer_id varchar(20) REFERENCES customers(customer_id),
+customer_id varchar(50) REFERENCES customers(customer_id),
 employee_id int REFERENCES employees(employee_id) not NULL,
 order_date date,
-ship_city varchar(30) not NULL
+ship_city varchar(50) not NULL
 )
