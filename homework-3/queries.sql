9db76c71c970dac8f4f5de3a6d25823612d749ff
rtangels <VanshinChaineezMaster@yandex.ru>
2023-06-12 21:00:53 +0300

    HW18.1

--- homework-3/queries.sql
+++ homework-3/queries.sql
@@ -1,16 +1,31 @@
 -- Напишите запросы, которые выводят следующую информацию:
 -- 1. Название компании заказчика (company_name из табл. customers) и ФИО сотрудника, работающего над заказом этой компании (см таблицу employees),
 -- когда и заказчик и сотрудник зарегистрированы в городе London, а доставку заказа ведет компания United Package (company_name в табл shippers)
+SELECT customers.company_name,first_name, last_name
+FROM orders
+INNER JOIN employees ON  orders.employee_id = employees.employee_id
+INNER JOIN customers ON  customers.customer_id = orders.customer_id AND customers.city=employees.city AND customers.city='London'
+INNER JOIN shippers ON  orders.ship_via=shippers.shipper_id AND shippers.company_name= 'United Package'
 
 
 -- 2. Наименование продукта, количество товара (product_name и units_in_stock в табл products),
 -- имя поставщика и его телефон (contact_name и phone в табл suppliers) для таких продуктов,
 -- которые не сняты с продажи (поле discontinued) и которых меньше 25 и которые в категориях Dairy Products и Condiments.
 -- Отсортировать результат по возрастанию количества оставшегося товара.
-
+SELECT products.product_name,units_in_stock,contact_name,suppliers.phone
+FROM products
+LEFT JOIN suppliers ON products.supplier_id = suppliers.supplier_id
+INNER JOIN categories ON categories.category_id = products.category_id AND (category_name = 'Dairy Products' OR category_name = 'Condiments')
+WHERE discontinued<>1 AND units_in_stock < 25
+ORDER BY units_in_stock ASC
 
 -- 3. Список компаний заказчиков (company_name из табл customers), не сделавших ни одного заказа
-
+SELECT customers.company_name
+FROM customers
+WHERE NOT EXISTS (SELECT * FROM orders WHERE orders.customer_id = customers.customer_id)
 
 -- 4. уникальные названия продуктов, которых заказано ровно 10 единиц (количество заказанных единиц см в колонке quantity табл order_details)
 -- Этот запрос написать именно с использованием подзапроса.
+SELECT DISTINCT product_name FROM products
+WHERE EXISTS (SELECT * FROM order_details WHERE order_details.product_id=products.product_id AND quantity=10)
+
